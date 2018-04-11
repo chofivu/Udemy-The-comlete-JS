780a4f4b2cd13f9c704436c1a0effaa851f304ed
Filip Vugrinec <f.vugrinec1@gmail.com>
2018-04-11 19:32:03 +0200

    variables

---
+++ 2-JS-Basics/script.js
@@ -0,0 +1,12 @@
+// lecture: variables
+var name = 'James';
+console.log(name);
+
+var lastName = 'Bond';
+console.log(lastName);
+
+var age = 37;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
