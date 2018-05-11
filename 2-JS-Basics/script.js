fa0536224d6b865f1738d19be9eace8277d91fce
Filip Vugrinec <f.vugrinec1@gmail.com>
2018-05-11 14:19:23 +0200

    Loops

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -274,9 +274,9 @@ if (john.indexOf('teacher') === -1) {
 */
 
 
-
+/*
 //////////////////////////////////////////////////
-// lecture: Objects
+// lecture: Objects 
 
 var john = {
     name: 'John',
@@ -309,6 +309,101 @@ jane['job'] = 'retired';
 jane['isMarried'] = true;
 
 console.log(jane);
+*/
+
+
+/////////////////////////////////////////////////
+// lecture: Objects & methods
+
+// v1.0
+/*
+var john = {
+    name: 'John',
+    lastName: 'Smith',
+    yearOfBirth: 1990,
+    job: 'teacher',
+    isMarried: false,
+    family: ['Jane', 'Mark', 'Bob'],
+    calculateAge: function(){
+        return 2018 - this.yearOfBirth;
+    }
+};
+
+
+//console.log(john.calculateAge());
+//console.log(john.calculateAge());
+
+var age = john.calculateAge();
+john.age = age;
+
+console.log(john);
+*/
+
+/*
+//v2.0
+var john = {
+    name: 'John',
+    lastName: 'Smith',
+    yearOfBirth: 1990,
+    job: 'teacher',
+    isMarried: false,
+    family: ['Jane', 'Mark', 'Bob'],
+    calculateAge: function(){
+        //return 2018 - this.yearOfBirth;
+        this.age = 2016 - this.yearOfBirth;
+    }
+};
+
+john.calculateAge();
+console.log(john);
+*/
+
+
+//////////////////////////////////////////
+// lecture: loops
+/*
+for (i=0; i<10; i++) {
+    console.log(i);
+}
+*/
+
+var names = ['John', 'Jane', 'Mary', 'Mark', 'Bob'];
+/*
+
+//for loops
+for (i = 0; i < names.length; i++){
+    console.log(names[i]);
+}
+
+for(i = names.length - 1; i >= 0; i--){
+    console.log(names[i]);
+}
+*/
+
+// while loops
+var i = 0
+while(i < names.length) {
+    console.log(names[i]);
+    i++;
+}
+
+
+for (i = 1; i<=5; i++) {
+    console.log(i);
+
+    if (i === 3){
+        break;
+    }
+}
+
+
+for (i = 1; i<=5; i++) {
+    if (i === 3){
+        continue;
+    }
+
+    console.log(i);
+}
 
 
 
